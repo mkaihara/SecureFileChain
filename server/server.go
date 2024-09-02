0f6029e01bb0e68c86a416dfdf92aeed98760b7b
Marcelo Kaihara <marcelo.kaihara@gmail.com>
2024-09-02 17:47:57 +0200

    Clean up the code

--- server/server.go
+++ server/server.go
@@ -339,43 +339,6 @@ func (s *Server) merkleRootHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func authenticate(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("token")
-		if err != nil {
-			if err == http.ErrNoCookie {
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		tokenStr := cookie.Value
-		claims := &Claims{}
-		token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
-			return jwtKey, nil
-		})
-
-		if err != nil {
-			if err == jwt.ErrSignatureInvalid {
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if !token.Valid {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		// If everything is good, proceed with the request
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	filesDir := "/app/downloads"
 	log.Printf("File storage directory on the server side: %s\n", filesDir)
